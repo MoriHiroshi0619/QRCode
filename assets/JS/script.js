32e9b391a8d5512870b43cf87fd1584336efbbbf
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2023-04-29 21:59:32 -0400

    Update script.js

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,4 +1,18 @@
 function GerarQRcode(){
-    let inputUsuario = document.querySelector('#link').ariaValueMax;
-    
+    let inputUsuario = document.querySelector('#link').value;
+    console.log(inputUsuario);
+    let GoogleChartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=500x500&chld=H&chl=';
+    let QRCodeContent = GoogleChartAPI + encodeURIComponent(inputUsuario);
+    document.querySelector('#QRCodeImage').src = QRCodeContent;
 }
+
+/*
+link para a documentação da API: https://developers.google.com/chart/infographics/docs/qr_codes?hl=en
+parametros da API obrigatórios
+cht=qr: especifica como um QRCode.
+chs=<width>x<height>: tamamho do QRcode
+chl=<data>: aqui é o conteudo que vai aparecer no QRCode que pode ser um link, texto,etc..
+
+parametros extra da API
+chld=<level>: nivel de correção de erro. enum(H, M, Q, L)
+*/
