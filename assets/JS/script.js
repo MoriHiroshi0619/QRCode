b401fcdd08a12215a60ec31ab4c3012c125d49c6
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2023-05-04 23:32:13 -0400

    updated

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,16 +3,23 @@ let btsGerar = document.querySelector('.bts-link');
 btsGerar.addEventListener('click', GerarQRcode);
 
 function GerarQRcode(){
-    console.log("apertado foi então funciona seu filho da puta!!!");
     let inputUsuario = document.querySelector('#link').value;
-    console.log(inputUsuario);
-    let GoogleChartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=250x250&chld=H&choe=UTF-8&chl=';
+    //console.log(inputUsuario);
+    let GoogleChartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=250x250&chld=L&choe=UTF-8&chl=';
+
+    let correcao = document.querySelector('#chld');
+    let chld = correcao.options[correcao.selectedIndex].value;
+    console.log(chld);
+
     let QRCodeContent = GoogleChartAPI + encodeURIComponent(inputUsuario);
+
+
     let imageQRcode = document.querySelector('#QRCodeImage');
     imageQRcode.src = QRCodeContent;
     imageQRcode.classList.add('.qrcode-appear');
     imageQRcode.style.opacity = 1;
-    console.log(QRCodeContent);
+
+    //console.log(QRCodeContent);
 }
 
 /*
